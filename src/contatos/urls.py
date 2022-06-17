914b0fc20a03a3e8d4aa80a515d1dafe6bc7b362
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-06-17 00:38:44 -0300

    feat: 🎸 implements search component

--- src/contatos/urls.py
+++ src/contatos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('search/', views.search, name='search'),
     path("<int:contact_id>", views.list_contact, name="list_contact"),
 ]
