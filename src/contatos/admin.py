be805a98cfcd572f3141bb9d8c83e157ac74c0f9
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-06-16 22:51:34 -0300

    feat: 🎸 small update on models

--- src/contatos/admin.py
+++ src/contatos/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Categoy, Contact
 
-# Register your models here.
+admin.site.register(Categoy)
+admin.site.register(Contact)
--- src/contatos/models.py
+++ src/contatos/models.py
@@ -1,17 +1,24 @@
+from datetime import datetime
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Categoy(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Contact(models.Model):
     name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255, blank=True, null=True)
-    phone = models.CharField(max_length=255, blank=True, null=True)
+    phone = PhoneNumberField(region="BR")
     email = models.EmailField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=datetime.now)
     description = models.TextField(blank=True, null=True)
     category = models.ForeignKey(Categoy, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
