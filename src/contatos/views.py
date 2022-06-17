c88ec24a8210fa23abcb9196ac2abe928329dd28
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-06-16 23:59:24 -0300

    feat: 🎸 update views of contact

--- src/contatos/views.py
+++ src/contatos/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Contact
 
 
 def index(request):
-    return render(request, "contatos/index.html")
+    contacts = Contact.objects.all()
+    return render(request, "contatos/index.html", {
+        "contacts": contacts
+    })
+
+
+def list_contact(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+    return render(request, "contatos/list_contact.html", {
+        "contact": contact
+    })
