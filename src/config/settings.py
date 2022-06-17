37bb5787974747e8cd409806e214bc4560588452
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-06-16 22:20:22 -0300

    feat: 🎸 put mysql on requirements and create connection

--- src/config/settings.py
+++ src/config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "USER": "root",
+        "PASSWORD": "123456",
+        "HOST": "127.0.0.1",
+        "PORT": "3002",
+        "NAME": "agenda",
     }
 }
 
