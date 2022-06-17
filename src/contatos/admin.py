4e97c3b118be7271af8f31c314054f8c0858f3b0
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-06-16 23:12:34 -0300

    test: 💍 create ContactAdmin

--- src/contatos/admin.py
+++ src/contatos/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import Categoy, Contact
 
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "last_name",
+        "phone",
+        "email",
+        "category",
+        "created_at",
+    )
+
+    list_display_links = ("name", "last_name")
+
+    list_per_page: int = 10
+
+    search_fields = ("name", "last_name", "phone", "email",)
+
+
 admin.site.register(Categoy)
-admin.site.register(Contact)
+admin.site.register(Contact, ContactAdmin)
