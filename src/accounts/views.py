85486266be8b8d30dda6f2b6abcbb5aec3f1778e
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-06-21 17:23:17 -0300

    feat: 🎸 refactor login view

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,89 +1,60 @@
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
-from django.core.validators import validate_email
+from .form import UserForm, UserLoginForm
+from .decorators import check_recaptcha
 
 
+@check_recaptcha
 def login(request):
     if request.user.is_authenticated:
         return redirect("contact_list")
     else:
         if request.method != "POST":
-            return render(request, "accounts/login.html")
+            form = UserLoginForm()
+            return render(request, "accounts/login.html", {
+                "form": form,
+                'site_key': settings.RECAPTCHA_PUBLIC_KEY
+            })
 
-        username = request.POST.get("username")
-        password = request.POST.get("password")
+        form = UserLoginForm(request.POST)
 
-        if not (username and password):
-            messages.error(request, "Por favor, preencha todos os campos.")
-            return render(request, "accounts/login.html")
+        user = auth.authenticate(
+            username=request.POST['username'],
+            password=request.POST['password']
+        )
 
-        user = auth.authenticate(username=username, password=password)
-
-        if not user:
-            messages.error(request, "Usuário ou senha incorretos.")
-            return render(request, "accounts/login.html")
-
-        auth.login(request, user)
-        messages.success(request, "Bem Vindo!")
-        return redirect("contact_list")
+        if user is not None:
+            auth.login(request, user)
+            return redirect("contact_list")
+        else:
+            messages.error(request, "Usuário ou senha inválidos")
+            return render(request, "accounts/login.html", {
+                "form": form,
+                'site_key': settings.RECAPTCHA_PUBLIC_KEY
+            })
 
 
 def register(request):
     if request.method != "POST":
         return render(request, "accounts/register.html")
 
-    first_name = request.POST.get("first_name")
-    last_name = request.POST.get("last_name")
-    username = request.POST.get("username")
-    email = request.POST.get("email")
-    password = request.POST.get("password")
-    re_password = request.POST.get("re_password")
-
-    if not (
-        first_name
-        and last_name
-        and username
-        and email
-        and password
-        and re_password
-    ):
-        messages.error(request, "Por favor, preencha todos os campos.")
+    form = UserForm(request.POST)
+    if not form.is_valid():
+        messages.error(request, "Nome de usuário ou e-mail inválidos.")
         return render(request, "accounts/register.html")
-
-    try:
-        validate_email(email)
-    except Exception:
-        messages.error(request, "Email inválido.")
-        return render(request, "accounts/register.html")
-
-    if User.objects.filter(email=email).exists():
-        messages.error(request, "Email já cadastrado.")
-        return render(request, "accounts/register.html")
-
-    if User.objects.filter(username=username).exists():
-        messages.error(request, "Nome de usuário já cadastrado.")
-        return render(request, "accounts/register.html")
-
-    if len(password) < 6:
-        messages.error(request, "Senha deve ter no mínimo 6 caracteres.")
-        return render(request, "accounts/register.html")
-
-    if password != re_password:
-        messages.error(request, "Senhas não conferem.")
-        return render(request, "accounts/register.html")
-
-    messages.success(request, "Usuário cadastrado com sucesso!")
 
     user = User.objects.create_user(
-        username=username,
-        email=email,
-        password=password,
-        first_name=first_name,
-        last_name=last_name,
+        first_name=form.cleaned_data.get("first_name"),
+        last_name=form.cleaned_data.get("last_name"),
+        username=form.cleaned_data.get("username"),
+        password=form.cleaned_data.get("password"),
+        email=form.cleaned_data.get("email"),
     )
     user.save()
 
+    messages.success(request, "Usuário criado com sucesso!")
     return redirect("login")
 
 
