796417522faaaf2abc40a140a6c566391352527c
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-06-21 20:27:10 -0300

    feat: 🎸 update form for registration account

--- src/accounts/form.py
+++ src/accounts/form.py
@@ -1,47 +1,80 @@
 from django import forms
 
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
-class UserForm(forms.ModelForm):
-    re_password = forms.CharField(
-        label="Confirmar senha",
-        widget=forms.PasswordInput,
-        required=True,
-    )
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'password', 'email']
-        widgets = {
-            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'username': forms.TextInput(attrs={'class': 'form-control'}),
-            'password': forms.PasswordInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
-        }
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2',
+        )
 
-    def clean_username(self):
-        username = self.cleaned_data['username']
-        if User.objects.filter(username=username).exists():
-            raise forms.ValidationError('Usuário Já existe')
-        return username
+    def clean_first_name(self):
+        first_name = self.cleaned_data['first_name']
+        if not first_name:
+            raise forms.ValidationError('O nome é obrigatório.')
+        if len(first_name) < 3:
+            raise forms.ValidationError(
+                'O nome deve ter no mínimo 3 caracteres.'
+            )
+        return first_name
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data['last_name']
+        if not last_name:
+            raise forms.ValidationError('O sobrenome é obrigatório.')
+        if len(last_name) < 3:
+            raise forms.ValidationError(
+                'O sobrenome deve ter no mínimo 3 caracteres.'
+            )
+        return last_name
 
     def clean_email(self):
         email = self.cleaned_data['email']
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError('Email Já existe')
+            raise forms.ValidationError('Este email já está cadastrado.')
         return email
 
-    def clean_password(self):
-        password = self.cleaned_data['password']
-        if len(password) < 6:
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        if not password1:
+            raise forms.ValidationError('A senha é obrigatória.')
+        if len(password1) < 8:
             raise forms.ValidationError(
-                'Senha deve ter no mínimo 6 caracteres')
-        if password != self.cleaned_data['re_password']:
-            raise forms.ValidationError(
-                'Senhas não conferem')
-        return password
+                'A senha deve ter no mínimo 8 caracteres.'
+            )
+        return password1
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if not password2:
+            raise forms.ValidationError('Confirme a senha.')
+        if password1 != password2:
+            raise forms.ValidationError('As senhas não conferem.')
+
+        return password2
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+
+        return user
 
 
 class UserLoginForm(forms.Form):
