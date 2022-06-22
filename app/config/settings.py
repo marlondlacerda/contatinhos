a5dd1bad5e42fddb184a44621a4f6be517420e0c
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-06-22 18:41:55 -0300

    feat: 🎸 create docker-compose.prod

--- app/config/settings.py
+++ app/config/settings.py
@@ -125,12 +125,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "templates/static"
-]
-STATIC_ROOT = STATICFILES_DIRS
+
+STATIC_ROOT = str(BASE_DIR / "templates/static")
+
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "media/"
 
